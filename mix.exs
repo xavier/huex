7685b3a947905c8ef07b43f063c6993ab8030779
Xavier Defrang <xavier.defrang@gmail.com>
2018-12-11 09:06:55 +0100

    Upgrade ex_doc

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Huex.Mixfile do
       {:nerves_ssdp_client, "~> 0.1", optional: true},
       {:exvcr, "~> 0.8.0", only: :test},
       {:earmark, "~> 1.0", only: :dev},
-      {:ex_doc, "~> 0.14", only: :dev}
+      {:ex_doc, "~> 0.19", only: :dev}
     ]
   end
 
