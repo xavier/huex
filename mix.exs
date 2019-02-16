4e9b8881dedcd095094eb7614ce5dfec95f7130b
Arijit Dasgupta <arijit.rivalslayer@gmail.com>
2019-02-16 16:06:27 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Huex.Mixfile do
   def project do
     [
       app: :huex,
-      version: "0.8.0",
+      version: "0.9.0",
       elixir: "~> 1.1",
       deps: deps(),
       description: description(),
